1f74667c9eb959084c1a24a6bab15f84ec32e743
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-03-08 23:08:50 +0530

    first commit

--- src/Awards.jsx
+++ src/Awards.jsx
@@ -1,7 +1,16 @@
 function Awards(){
-    const awards = ['Ballon dor', 'Golden boot'];
+    const awards = [{name: 'Ballon dor', year: 2023}, 
+        {name: 'Golden boot', year: 2019}];
 
-    return(awards);
+
+    // awards.sort((a, b)=> a.name.localeCompare(b.name)); //Alphabetical sort
+    // awards.sort((a, b)=> b.name.localeCompare(a.name)); //Rev Alphabetical sort
+    // awards.sort((a, b)=> a.year.localeCompare(b.year)); //Numerical sort
+    // awards.sort((a, b)=> b.year.localeCompare(a.year)); //Rev Numerical sort
+    
+    const listItems = awards.map(award => <li key={award.name}>{award.name}: &nbsp; <b>{award.year}</b> </li>);
+        // make sure key is unique
+    return(<ul>{listItems}</ul>);
 }
 
 export default Awards
