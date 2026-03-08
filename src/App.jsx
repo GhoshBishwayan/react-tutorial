bf7c9c07b0b86797ed86c384c8d623e79f9584fb
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-03-08 23:09:18 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import Header from './header.jsx'
 // import Footer from './Footer.jsx'
 // import Food from './Food.jsx'
+import Awards from './Awards.jsx'
 import Card from './Card.jsx'
 import Button from './Button/Button.jsx';
 import Student from './Student.jsx'
@@ -21,6 +22,7 @@ function App() {
     <Button2/> */}
     <Card/>
     <Button/>
+    <Awards/>
     <Achivements cupName="World Cup" year = {2022} playerOfTheTourny={true} age={35} />
     <Achivements cupName="UCL" year = {2015} playerOfTheTourny={true} age={26} />
     <Achivements year={2023}/>
