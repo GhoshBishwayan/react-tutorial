49d3c757cd5e3027c9a0224f02c835df79b69339
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-03-23 19:43:00 +0530

    23-03-2026

--- src/Comments/Comments.jsx
+++ src/Comments/Comments.jsx
@@ -4,12 +4,17 @@ import styles from './Comments.module.css'
 function Comments(){ 
     const [name, setName] = useState("Guest");
     const [comment, setComment] = useState("");
+    const [payment, setPayment] = useState("");
     
     function handleNameChange(event){
         setName(event.target.value);
     }
     function forMessi(event){
         setComment(event.target.value);
+    }
+
+    function commentValue(event){
+        setPayment(event.target.value);
     }
 
     return( 
@@ -20,6 +25,14 @@ function Comments(){
                 <p className={styles.userName}>Name: {name}</p>
                 <p className={styles.userComment}>Comment: {comment}</p>
             </div>
+                <p className={styles.userComment}>Payment: {payment}</p>
+
+            <select value={payment} onChange={commentValue}>
+                <option value="">Select an option</option>
+                <option value="Visa">Visa</option>
+                <option value="Mastercard">Mastercard</option>
+                <option value="Bitcoin">Bitcoin</option>
+            </select>
         </div>
     )
 }
